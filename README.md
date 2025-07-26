9623d5981f4e9334b39ba7472fea93df971e7c10
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2025-07-26 16:46:58 +0900

    build: add shadcn ui

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # WXT + React
 
-This template should help get you started developing with React in WXT.
+This template should help get you started developing with React in WXT. (refer to [WXT + React + Tailwind + Shadcn](https://github.com/wxt-dev/examples/blob/main/examples/react-shadcn/README.md))
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   vite: () => ({
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   }),
 });
